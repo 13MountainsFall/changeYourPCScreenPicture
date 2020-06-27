e2d8cf53c6bf7af0e469b3c76c280a005fc41ccc
13mountainsfall <qiuyebai123@gmail.com>
2020-06-27 15:59:01 +0800

    [qiuyebai]#NA 获得开机画面的壁纸

---
+++ ChangeFileName.java
@@ -0,0 +1,44 @@
+package com.gao;
+
+
+import java.io.*;
+
+/**
+ * gaoyuxin
+ * qiuyebai123@gmail.com
+ */
+public class ChangeFileName {
+    //        源文件路径
+    static String str1 = "E:\\E02图片\\AllBiZhi01";
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        //获取 加jpg
+        //获取源文件路径下 每一张图片的图片名，调用下面的方法
+        String fileName = "";
+
+        //获取所有文件名list
+        File file = new File(str1);
+        File[] list = file.listFiles();
+        System.out.println(list);//[Ljava.lang.String;@1b6d3586
+
+        //gei all names for String
+        for (File item : list) {
+            if(!item.isDirectory() && !item.getName().endsWith(".jpg") && !item.getName().endsWith(".txt") && !item.getName().endsWith(".bat")){
+                System.out.println(item);
+                System.out.println(str1+"\\"+item.getName()+".jpg");
+                item.renameTo(new File(str1+"\\"+item.getName()+".jpg"));
+                System.out.println("改名成功");
+                System.out.println("---------------------");
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ cmd.bat
@@ -0,0 +1,7 @@
+
+
+@echo off
+D:
+cd D:\WorkspaceDemos\DataStructures\Codes\DataStructureAndMath\out\production\mytest
+java com.gao.ChangeFileName
+@echo on
